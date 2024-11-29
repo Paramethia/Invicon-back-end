587d45c716dbdef8fecd91009d39add4418f7c50
Kyrin <143742919+Paramethia@users.noreply.github.com>
2024-11-29 21:01:38 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,318 @@
+require('dotenv').config();
+const cors = require('cors');
+const path = require('path');
+const express = require('express');
+const mongoose = require('mongoose');
+const Invites = require('./models/Invitates');
+const Users = require('./models/Users');
+const { MailerSend, EmailParams, Sender, Recipient } = require('mailersend');
+
+const ex = express();
+ex.use(express.json());
+ex.use(cors({
+    origin: "https://www.invicon.lol",
+    methods: ['GET', 'POST'],
+    credentials: true
+}));
+
+let clusterURL = "mongodb+srv://paramethia:PCx48Hh.u7e-_zM@mycluster.ahfaufe.mongodb.net/invicon?retryWrites=true&w=majority&appName=MyCluster";
+
+mongoose.connect(clusterURL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    tlsInsecure: true,
+});
+
+// Function to clear the database
+const clearDatabase = async () => {
+    try {
+        // Remove all documents from the collection
+        await Users.deleteMany({});
+        await Invites.deleteMany({});
+        console.log('Database cleared successfully');
+    } catch (error) {
+        console.error('Error clearing the database:', error);
+    } finally {
+        // Disconnect from the database
+        //mongoose.disconnect();
+    }
+};
+
+//clearDatabase();
+
+let mailerSend = new MailerSend({
+    apiKey: process.env.MailerSEND_API_KEY,
+});
+
+let sendFrom = new Sender("admin@invicon.lol", "Invicon");
+
+// Register page function
+
+ex.post('/register', async (req, res) => {
+    const { username, email, password, usedInvite } = req.body;
+
+    try {
+        // Check if the username is already taken
+        const userName = await Users.findOne({ username });
+        if (userName) {
+            return res.json("Username already taken.");
+        }
+
+        // If email is provided, check if it's already registered
+        if (email) {
+            const userEmail = await Users.findOne({ email });
+            if (userEmail) {
+                return res.json("Account already registered.");
+            }
+        }
+
+        // Create the new user object, include email only if provided
+        const newUserData = {
+            username,
+            password,
+            usedInvite
+        };
+        
+        // Add the email field only if it's provided
+        if (email) {
+            newUserData.email = email;
+        }
+        
+        // Save the user
+        const newUser = new Users(newUserData);
+        await newUser.save();
+
+        res.json("Registered.");
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({ message: "Internal server error." });
+    }
+});
+
+
+// Log in page function
+
+ex.post('/login', async (req, res) => {
+    const { username, password } = req.body;
+    try {
+        const user = await Users.findOne({ username });
+        if (user) {
+            if (user.password === password) {
+                return res.json("Correct username and password.");
+            } else {
+                return res.json("");
+            }
+        } else {
+            return res.json("Unregistered user.");
+        }
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+// Password reset request page function
+
+ex.post('/request-password-reset', async (req, res) => {
+    const { email } = req.body;
+
+    try {
+        const user = await Users.findOne({ email });
+        if (!user) {
+            return res.status(404).json({ message: "User not found" });
+        }
+
+        const token = crypto.randomBytes(32).toString('hex');
+        user.resetPasswordToken = token;
+        user.resetPasswordExpires = Date.now() + 3600000; // 1 hour
+        await user.save();
+
+        let resetLink = `https://invicon.lol/reset?token=${token}`;
+
+        const recipients = [new Recipient(email, "User")];
+        const emailParams = new EmailParams()
+            .setFrom(sentFrom)
+            .setTo(recipients)
+            .setReplyTo(sentFrom)
+            .setSubject("Password Reset")
+            .setHtml(`
+                <h1> Invicon password reset request </h1>
+                <p> You are receiving this because you (or someone else) have requested the reset of the password for your account. </p>
+                <p> Please click on the following link to reset your password: </p>
+                <p><a href="${resetLink}">${resetLink}</a></p>
+                <p>If you did not request this, please ignore this email and your password will remain unchanged.</p>
+            `);
+
+        await mailerSend.email.send(emailParams);
+        res.json({ message: "Password reset email sent" });
+    } catch (err) {
+        console.error("Error sending email:", err);
+        res.status(500).json({ message: "Error sending email" });
+    }
+});
+
+// Passwrod reset page function
+
+ex.post('/reset-password', async (req, res) => {
+    const { token, newPassword } = req.body;
+
+    try {
+        const user = await Users.findOne({
+            resetPasswordToken: token,
+            resetPasswordExpires: { $gt: Date.now() },
+        });
+        if (!user) {
+            return res.status(400).json("Password reset token is invalid or has expired");
+        }
+
+        user.password = newPassword;
+        user.resetPasswordToken = undefined;
+        user.resetPasswordExpires = undefined;
+        await user.save();
+        res.json("Password has been reset");
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+// Home page functions
+
+let generateInviteId = () => {
+    // Logic to generate a unique invite Id
+    return `${Math.random().toString(35).substring(2, 10)}`;
+}
+
+ex.post('/generate-invite', async (req, res) => {
+    const {username} = req.body;
+    const inviteId = generateInviteId();
+
+    try {
+        let inviter = await Invites.findOne({ username });
+
+        if (inviter) {
+            // If an inviteId already exists for the username, return the existing Id with link
+            return res.json({ inviteLink: `https://invicon.lol/register?inviteId=${inviter.inviteId}` });
+        } else {
+             const newInvite = new Invites({
+                username,
+                inviteId: inviteId
+            });
+        
+            await newInvite.save();
+            res.json({ inviteLink: `https://invicon.lol/register?inviteId=${newInvite.inviteId}` });
+        }
+    } catch (err) {
+        console.error('Error generating invite link:', err);
+        res.status(500).json({ message: "Internal server error" });
+    }
+});
+
+const tierUpdate = (inviter) => {
+    if (inviter.invites >= 100) {
+        inviter.tier = 8;
+    } else if (inviter.invites >= 85) {
+        inviter.tier = 7;
+    } else if (inviter.invites >= 70) {
+        inviter.tier = 6;
+    } else if (inviter.invites >= 50) {
+        inviter.tier = 5
+    } else if (inviter.invites >= 35) {
+        inviter.tier = 4;
+    } else if (inviter.invites >= 20) {
+        inviter.tier = 3;
+    } else if (inviter.invites >= 10) {
+        inviter.tier = 2;
+    } else if (inviter.invites >= 5) {
+        inviter.tier = 1;
+    } else {
+        inviter.tier = 0;
+    }
+};
+
+// Home, Dahsboard and Leaks page functions
+
+ex.post('/invite-check', async (req, res) => {
+    const { username, inviteId } = req.body;
+
+    try {   
+        let inviter = await Invites.findOne({ inviteId });
+            
+        if (inviter) {
+            // Increment the invites count and assign the user who used it
+
+            inviter.invites++; 
+            inviter.usedBy.push({ username });
+
+            tierUpdate(inviter);
+
+            await inviter.save();
+            res.json({ message: "Code found and updated data." });
+        } else { 
+            res.status(402).json({ message: "Invalid invite code." });
+        }
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+ex.post('/invite-data', async (req, res) => {
+    const {username} = req.body;
+
+    try {
+        let inviter = await Invites.findOne({ username });
+
+        if (!inviter) {
+            return res.status(404).json({ message: 'Invite data not found' });
+        }
+
+        res.json({ invites: inviter.invites, tier: inviter.tier });
+    } catch (error) {
+        console.error('Error fetching invite data:', error);
+    }
+});
+
+ex.post('/invites', async (req, res) => {
+    const { username } = req.body;
+
+    try {
+        let inviter = await Invites.findOne({ username });
+
+        if (inviter) {
+            if (inviter.invites >= 1) {
+                res.json({ invitees: inviter.usedBy });
+            } else {
+                res.json({ message: "No invites yet." });
+            }
+        } else {
+            res.status(402).json({ message: "Inviter not found." });
+        } 
+    } catch (error) {
+        console.error("Error checking invites:", error);
+        res.status(500).json({ message: "Internal server error" });
+    }
+});
+
+ex.post('/getTier', async (req, res) => {
+    const { username } = req.body;
+
+    try {
+        let inviter = await Invites.findOne({ username });
+
+        if (inviter) {
+            res.json({ message: "User found.", tier: inviter.tier });
+        } else {
+            res.status(402).json({ message: "User not found." });
+        }
+    } catch (error) {
+        res.status(500).json({ error: 'Server error' });
+    }
+});
+
+
+const clientPage = path.join(__dirname + '/client/build/index.html');
+ex.get('*', (req, res) => {
+    res.sendFile(clientPage);
+});
+
+ex.listen(2004, () => {
+    console.log("Server listening on http://128.0.3.5:2004");
+});
---
+++ models/Users.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+
+const Users = new mongoose.Schema({
+    username: { type: String, required: true },
+    email: { type: String, required: false },
+    password: { type: String, required: true },
+    usedInvite: { type: String },
+    resetPasswordToken: { type: String },
+    resetPasswordExpires: { type: Date }
+});
+
+const Invitates = mongoose.model('Users', Users);
+
+module.exports = FormDataModel;
